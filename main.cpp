1a914bcf59cd3867947222da64994b336e1605d0
vuky <mandirvojtech@gmail.com>
2026-04-26 19:05:56 +0200

    dobry den cely jsem to kopiroval z jineho souboru protoze mi v nem neslo nic buiildnout a ani ai nevedelo co a ani nesel udelat repozitar tak jen abyste si nemyslel ze jsem to zkopiroval z ai mejte hezky den

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include <vector> // pridani vektoru
 using namespace std;
+struct Player { // funkce na postavu s pomoci ai abych se naucil s tim co je vektor
+    string name;
+    int hp;
+    int att;
+    int x,y;
+    vector<string> sprite = { // vykresleni postavy
+        " O ",
+        "/|\\",
+        "/ \\"
+    };
+};
 int main() {
-    int hodnota = 0;
-    cout << "Hello world!" << endl;
-
-    return 0;
+    cout << "Hello World!" << endl;
 }
