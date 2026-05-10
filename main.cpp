b55bef7dc4f0489e9853166cea52df82921a6711
vuky <mandirvojtech@gmail.com>
2026-05-10 16:37:15 +0200

    dobry den zase jsem to vsechno predelal ale tentokrat uz vim jak to udelat

--- main.cpp
+++ main.cpp
@@ -1,29 +1,29 @@
 #include <iostream>
+#include <ctime>
 #include <vector> // pridani vektoru
 using namespace std;
-struct Player { // funkce na postavu s pomoci ai abych se naucil s tim co je vektor a struct znamená vlastní proměná co si muzu vytvorit
-    string name;
-    int hp;
-    int att;
-    int gold;
-};
-struct Monster { // na zapis promenych
-    string name;
-    int hp;
-    int att;
-    char type; // na typy monster atdd
-};
-struct Item {
-    string name;
-    int heal;
-    int price;
-};
-// tedka napisu funkci na overeni vstupu na rozsah jeste urcim jaky ted nevim
-int kontrola(int min, int max) {
-    int vyber;
-    while (!(cin >> vyber) || vyber < min || vyber > max) // kdyz se neco zapise tak jestli to je minimum a mazimum vyberu
+// promene hrace
+int hracmaxHP, hracHP, hracutok, hracmaxmana, hracmana;
+int zlato = 0, levelhrace = 1, hracXP = 0;
 
-};
+
 int main() {
+    srand(time(NULL)); // náhoda ve výběru
+
+    // jak se vybere classa
+    while (true) {
+        cout << "Vyberte si postavu mlady pane:\n";
+        cout << "1) Paladin (Zivot 5, Utok 3, Mana 5)\n";
+        cout << "2) Lovec (Zivot 4. Utok 4, Mana 3)\n";
+        cout << "3) Mag (Zivot 3, Utok 2, Mana 7) \n";
+        cout << "4) Warlock (Zivot 4, Utok 3, Mana 5)\n";
+        cout << "Volba: ";
+        int volba; cin >> volba;
+        if (volba == 1) {
+            hracmaxHP = 5; hracutok = 3; hracmaxmana = 5;
+            cout << "Class Paladin -> Zivot 5/5, Utok 3, Mana 5/5... \n";
+            cout << "Schopnosti: Uder (3 dmg), Uder svetlem (1 vsem a -2 many), Uzdraveni (+1 HP a -1 mana) \n";
+        }
+    }
 
 }
